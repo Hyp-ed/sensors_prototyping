6a26cec30fce76cc23fe081d179ad9f9702455ab
gregorydayao12 <s1814658@ed.ac.uk>
2019-09-28 22:24:10 +0100

    separate imu files

--- src/sensors/imu_i2c.cpp
+++ src/sensors/imu_i2c.cpp
@@ -20,7 +20,7 @@
 #include <algorithm>
 #include <vector>
 
-#include "sensors/imu.hpp"
+#include "sensors/imu_spi.hpp"
 #include "utils/concurrent/thread.hpp"
 #include "utils/math/statistics.hpp"
 
